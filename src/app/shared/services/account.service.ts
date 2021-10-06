9ae7a9c16a35c24c585b0e0a5787ac2a1e0c7c8b
Luan Eduardo da Costa <luan1346@gmail.com>
2021-10-05 23:02:38 -0300

    Create the account service

---
+++ src/app/shared/services/account.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs'
+
+type Account = {
+  id: number
+  name: string
+  email: string
+  password: string
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AccountService {
+  private baseUrl = 'http://localhost:3000/account'
+
+  constructor(private http: HttpClient) {}
+
+  login(email: string, password: string): Observable<Account[]> {
+    const params = new URLSearchParams({ email, password })
+    return this.http.get<Account[]>(`${this.baseUrl}?${params}`)
+  }
+
+  saveAccountSession(account: Account) {
+    localStorage.setItem(
+      '@PAY_FRIENDS/ACCOUNT_SESSION',
+      JSON.stringify(account),
+    )
+  }
+
+  logout() {
+    localStorage.clear()
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { HttpClientModule } from '@angular/common/http'
 
 import { IconsModule } from './icons/icons.module'
 import { InputComponent } from './components/input/input.component'
@@ -7,7 +8,7 @@ import { ButtonComponent } from './components/button/button.component'
 
 @NgModule({
   declarations: [InputComponent, ButtonComponent],
-  imports: [CommonModule, IconsModule],
+  imports: [CommonModule, IconsModule, HttpClientModule],
   exports: [IconsModule, InputComponent, ButtonComponent],
 })
 export class SharedModule {}
